307bed4a1938fd4de6eafbcebe055f9fea38c1cf
Roman Gassmann <admin@gr-solutions.net>
2013-03-22 11:48:04 +0100

    added goldsteinprice function

--- code/goldsteinprice.c
+++ code/goldsteinprice.c
@@ -1,26 +1,27 @@
 /*
- * rosenbrock2d.c -- rosenbrock function for testing
+ * goldsteinprice.c -- rosenbrock function for testing minima is at f(0,-1) = 0 for -2 <= x,y <= 2 
  *
  * (c) 2013 Roman Gassmann, HSR
  */
 #include <optim.h>
 #include <math.h>
 
-static double  min[2] = { -50.0, -50.0 };
-static double	max[2] = { 50.0, 50.0 };
+static double  min[2] = { -2.0, -2.0 };
+static double	max[2] = { 2.0, 2.0 };
 
 static inline double	sqr(double x) {
 	return x * x;
 }
 
-static double	rosenbrock2d_function(optim_t *optim, double *x) {
-	return 500 - 20*x[0] - 26*x[1] -4*x[0]*x[1] +4*sqr(x[0]) + 3*sqr(x[1]);
+static double	goldsteinprice_function(optim_t *optim, double *x) {
+	return ( 1 + sqr( x[0]+x[1]+1 ) * ( 19 - 14*x[0] + 3*sqr(x[0]) - 14*x[1] + 6*x[0]*x[1] + 3*sqr(x[1]) ) ) *
+		( 30 + sqr( 2*x[0] - 3*x[1] )*( 18 - 32*x[0] + 12*sqr(x[0]) + 48x[1] - 36*x[0]*x[1]+ 27*sqr(x[1]) ) );
 }
 
 optim_t	prob3 = {
 	.n = 2,
 	.min = min,
 	.max = max,
-	.f = rosenbrock2d_function,
+	.f = goldsteinprice_function,
 	.needs_deallocation = 0,
 };
